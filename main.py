be463b709c5c7e87858f018ff3470b49d7eccf06
zbuhrer <zbuhrer@gmail.com>
2024-05-17 13:15:24 -0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 import flet as ft
 
-class VisionLotus(ft.UserControl):
-    def __init__(self):
-        
-        self.appbar = ft.AppBar(
-            leading=ft.PopupMenuButton(icon=ft.icons.HOME_OUTLINED,items=[
-                ft.PopupMenuItem(text="Auction Information",on_click=self.show_auction_info),
-                ft.PopupMenuItem(text="Class Schedule",on_click=self.show_class_schedules)
-                ]),
-            title=ft.Text("Vision Lotus", style=ft.TextStyle(color=ft.colors.RED)),
-            center_title=False,
-            bgcolor=ft.colors.BLACK,
-            actions=[
-                ft.PopupMenuButton(icon=ft.icons.MENU,items=[
-                ft.PopupMenuItem(text="Presenter Application"),
-                ft.PopupMenuItem(text="Volunteer Application"),
-                ft.PopupMenuItem(text="Sponsorship Application"),
-                ft.PopupMenuItem(text="Vendor Application"),])])
-            
+
+def main(page: ft.Page) -> None:
     def show_auction_info(self, e):
         print("Auction information page opened!")
 
-    def show_class_schedules(self, e):
-        print("Class schedules page opened!")
+    def show_event_schedules(self):
+        print("Event schedules page opened!")
+        
+        appbody.value="Event schedules page placeholder."
+        page.update()
+        
+        return 
 
     def show_food_truck_schedule(self, e):
         print("Food truck schedule page opened!")
@@ -33,10 +22,28 @@ class VisionLotus(ft.UserControl):
     def show_special_guests_and_speakers(self, e):
         print("Special guests and speakers page opened!")
 
-def main(page: ft.Page) -> None:
-    app = VisionLotus()
-    page.appbar = app.appbar
-    page.add(ft.Text("Vision Lotus!", style=ft.TextStyle(color=ft.colors.RED)))
+    appbar = ft.AppBar(
+        leading=ft.PopupMenuButton(icon=ft.icons.HOME_OUTLINED,items=[
+            ft.PopupMenuItem(text="Event Schedule",on_click=show_event_schedules),
+            ft.PopupMenuItem(text="Map"),
+            ft.PopupMenuItem(text="Auction Information",on_click=show_auction_info),
+            ]),
+        title=ft.Text("Vision Lotus", style=ft.TextStyle(color=ft.colors.RED)),
+        center_title=False,
+        bgcolor=ft.colors.BLACK,
+        actions=[
+            ft.PopupMenuButton(icon=ft.icons.MENU,items=[
+            ft.PopupMenuItem(text="Presenter Application"),
+            ft.PopupMenuItem(text="Volunteer Application"),
+            ft.PopupMenuItem(text="Sponsorship Application"),
+            ft.PopupMenuItem(text="Vendor Application"),])])
+    
+    appbody = ft.Text(value="Vision Home Page Placeholder")
+ 
+
+
+    page.appbar = appbar
+    page.add(appbody)
 
 if __name__ == "__main__":
     ft.app(target=main)
